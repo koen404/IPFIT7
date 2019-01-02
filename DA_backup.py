ad0cc453fca3a32f07c34b018bf8b5f56e9ca893
koen <koenvandersijs@hotmail.com>
2019-01-02 20:08:03 +0100

    Can now logon via ssh to DA and add the back-up script to a file.

--- DA_backup.py
+++ DA_backup.py
@@ -3,6 +3,7 @@ import sys
 import ftplib
 import os
 import main
+import time
 from contextlib import closing
 
 
@@ -11,16 +12,35 @@ class DA_backup:
         self.client = None
         self.Log = main.Main().Log()
         self.main = main.Main()
-
+        self.shell = None
     def back_up(self, host, username, password):
         self.client = paramiko.SSHClient()
-        self.client.load_system_host_keys()
-        self.client.connect(host, username=username, password=password)
-        command = "echo \"action=backup&append%5Fto%5Fpath=nothing&database%5Fdata%5Faware=yes&email%5Fdata%5Faware=yes&local%5Fpath=%2Fhome%2Fadmin%2Fadmin%5Fbackups&owner=admin&type=admin&value=multiple&when=now&where=local&who=all\" >> /usr/local/directadmin/data/task.queue"
-        (stdin, stdout, stderr) = self.client.exec_command(command)
-        for line in stdout.readlines():
-            print(line)
-        self.client.close()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            self.client.connect(hostname=host, port=13370, username=username, password=password)
+            rootPass = input('Please enter the root password of the server:')
+            print(rootPass)
+            # command = "echo \"action=backup&append%5Fto%5Fpath=nothing&database%5Fdata%5Faware=yes&email%5Fdata%5Faware=yes&local%5Fpath=%2Fhome%2Fadmin%2Fadmin%5Fbackups&owner=admin&type=admin&value=multiple&when=now&where=local&who=all\" >> /usr/local/directadmin/data/task.queue"
+            command = 'echo \'Hallo test\'>> test1'
+            stdin, stdout, stderr = self.client.exec_command('su -c \"' + command + '\"')
+            stdin.write(rootPass+'\n')
+            stdin.flush()
+            print(stdout.readlines())
+
+        except Exception as e:
+            print('Connection failed')
+            print(e)
+    #
+    # def sendcommand(self, command):
+    #     time.sleep(1)
+    #     self.shell.recv(1024)
+    #     self.shell.send(command + '\n')
+    #     while not self.shell.recv_ready():  # Wait for the server to read and respond
+    #         time.sleep(0.1)
+    #     time.sleep(0.3)
+    #     output = self.shell.recv(1024)  # read in
+    #     # print(output)
+    #     time.sleep(0.1)
 
     def download(self,username,password, host):
         with closing(ftplib.FTP()) as ftp:
@@ -55,13 +75,12 @@ class DA_backup:
                     self.Log.info("Downloading File: " + filename)
                     localfile = os.path.join(sys.path[0], 'download', filename)
 
-                    ftp.retrbinary("RETR "+ filename,open(localfile,'wb').write, 8*1024)
+                    ftp.retrbinary("RETR " + filename, open(localfile, 'wb').write, 8*1024)
                     print(self.main.bereken_hash(localfile))
                     print("Downloading complete")
 
                 ftp.quit()
             except ftplib.error_perm:
                 print("error")
-
 
 
