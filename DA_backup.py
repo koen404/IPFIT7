71d75fd3f716330cba8b463ccafe5fd42d21d130
koen <koenvandersijs@hotmail.com>
2019-01-02 14:26:49 +0100

    The download function is now fully functional and will download the back-up file. The back-up function needs to be tested and changed so that it will allow to create back-ups on a single user bases instead of creating a back-up for all users.

--- DA_backup.py
+++ DA_backup.py
@@ -1,19 +1,21 @@
 import paramiko
 import sys
 import ftplib
-import logger
+import os
+import main
 from contextlib import closing
+
+
 class DA_backup:
     def __init__(self):
         self.client = None
-        self.Log = logger.logger().log()
+        self.Log = main.Main().Log()
+        self.main = main.Main()
 
-    def connect( self, host, username, password):
+    def back_up(self, host, username, password):
         self.client = paramiko.SSHClient()
         self.client.load_system_host_keys()
-        self.client.connect(host, username=username,password=password)
-
-    def back_up(self):
+        self.client.connect(host, username=username, password=password)
         command = "echo \"action=backup&append%5Fto%5Fpath=nothing&database%5Fdata%5Faware=yes&email%5Fdata%5Faware=yes&local%5Fpath=%2Fhome%2Fadmin%2Fadmin%5Fbackups&owner=admin&type=admin&value=multiple&when=now&where=local&who=all\" >> /usr/local/directadmin/data/task.queue"
         (stdin, stdout, stderr) = self.client.exec_command(command)
         for line in stdout.readlines():
@@ -26,26 +28,38 @@ class DA_backup:
                 ftp.connect(host,)
                 ftp.login(username,password)
                 ftp.set_pasv(True)
-                files = ftp.nlst()
                 ftp.cwd('/backups')
                 files = ftp.nlst()
-                i = 0
-                for file in files[2:]:
+                download_index = None
 
-                    i += 1
-                    print(str(i) +" " + file)
-                download_index= int(input("What file do you want to download?"))
-                filename= files[download_index+1].lstrip()
-                print("Downloading File:"+filename)
-                self.Log.info("Downloading File: " + filename)
-                localfile = filename
+                while download_index != 0:
+                    i = 0
+                    for file in files[2:]:
+                        i += 1
+                        print(str(i) + " " + file)
+                    print('0 Exit')
+                    index_input = False
+                    while index_input is False:
+                        download_index = input("What file do you want to download?")
+                        try:
+                            val = int(download_index)
+                            download_index = val
+                            index_input = True
+                        except ValueError:
+                            self.Log.error("Wrong Index number")
 
-                ftp.retrbinary("RETR "+ filename,open(localfile,'wb').write, 8*1024)
-                print("Downloading complete")
+                    if download_index == 0:
+                        continue
+                    filename = files[download_index+1].lstrip()
+                    print("Downloading File:"+filename)
+                    self.Log.info("Downloading File: " + filename)
+                    localfile = os.path.join(sys.path[0], 'download', filename)
+
+                    ftp.retrbinary("RETR "+ filename,open(localfile,'wb').write, 8*1024)
+                    print(self.main.bereken_hash(localfile))
+                    print("Downloading complete")
 
                 ftp.quit()
-
-
             except ftplib.error_perm:
                 print("error")
 
