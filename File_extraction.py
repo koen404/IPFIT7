0dfb620faae5f6929c4d44f6a3c27b7c879377d5
koen <koenvandersijs@hotmail.com>
2019-01-04 15:41:21 +0100

    The script will now also calculate the hash of all the extracted files and write them to a csv file. Also the script will check if the foldername already exist and print a warning so that files cannot be overwritten.

--- File_extraction.py
+++ File_extraction.py
@@ -2,6 +2,8 @@ import main
 import tarfile
 import os
 import sys
+import csv
+from time import gmtime, strftime
 
 class FileExtraction:
     def __init__(self):
@@ -9,18 +11,27 @@ class FileExtraction:
         self.Log = main.Main().Log()
         self.downloadPath = main.Main().download_path
         self.extractPath = os.path.join(sys.path[0], "extract")
-        if not os.path.exists(self.extractPath):
+        if os.path.exists(self.extractPath):
+            self.warning_bool = True
+        elif not os.path.exists(self.extractPath):
             os.makedirs(self.extractPath)
+            self.warning_bool = False
 
     def extract(self, filename):
-        if filename == '*':
-            self.extractAll()
-        else:
-            print(os.path.join(self.downloadPath, filename))
-            self.extractFile(os.path.join(self.downloadPath, filename))
+        if self.warning_bool is True:
+            print('Warning output folder already hold files either remove them or rename the folder')
+            print('Exiting script')
+            self.Log.warning('Output folder:' + self.extractPath + 'already hold files')
+
+        elif self.warning_bool is False:
+            if filename == '*':
+                self.extractAll()
+            else:
+                print(os.path.join(self.downloadPath, filename))
+                self.extractFile(os.path.join(self.downloadPath, filename))
 
     def extractFile(self, file):
-        if os.path.exists(file):
+        if os.path.exists(file) and self.warning_bool is False:
             if file.endswith('tar.gz'):
                 self.Log.info("Opening tar.gz file:" + file)
                 tar = tarfile.open(file, 'r:gz')
@@ -37,6 +48,9 @@ class FileExtraction:
                 self.Log.info('Extraction complete')
                 print('Extraction complete')
                 tar.close()
+                print('Calculating hashes of extracted files')
+                self.calculateHash(finalPath, dirname)
+
 
     def extractAll(self):
         listOfFile = os.listdir(self.downloadPath)
@@ -48,3 +62,17 @@ class FileExtraction:
                 self.extractFile(fullPath)
             else:
                 print('this is not a tar.gz file, skipping')
+
+    def calculateHash(self, path, dirname):
+        hashFile = os.path.join(path, dirname + '.csv')
+        with open(hashFile, 'w') as file:
+            filewriter = csv.writer(file,delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            filewriter.writerow(['Date', 'Filename', 'Hash'])
+            for path, subdirs, files in os.walk(path):
+                for name in files:
+                    print(os.path.join(path, name))
+                    hash = main.Main().bereken_hash(os.path.join(path, name))
+                    filewriter.writerow([str(strftime("%Y-%m-%d %H:%M:%S", gmtime())), os.path.join(path, name), str(hash)])
+            # filewriter.close()
+
