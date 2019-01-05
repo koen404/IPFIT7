c717f1f83a5b0a0a9b686b0ff2f0eac030d4b2c7
koen <koenvandersijs@hotmail.com>
2019-01-05 15:38:14 +0100

    Made some changes to the set-up of this script. Because the case folder is not statically defined so it will need the variable from the main class.

--- File_extraction.py
+++ File_extraction.py
@@ -5,33 +5,31 @@ import sys
 import csv
 from time import gmtime, strftime
 
+
 class FileExtraction:
     def __init__(self):
         self.filename = None
+
+        self.extract_path = None
+        self.download_path = None
+
         self.Log = main.Main().Log()
-        self.downloadPath = main.Main().download_path
-        self.extractPath = os.path.join(sys.path[0], "extract")
-        if os.path.exists(self.extractPath):
-            self.warning_bool = True
-        elif not os.path.exists(self.extractPath):
-            os.makedirs(self.extractPath)
-            self.warning_bool = False
+        self.extractPath = None
 
-    def extract(self, filename):
-        if self.warning_bool is True:
-            print('Warning output folder already hold files either remove them or rename the folder')
-            print('Exiting script')
-            self.Log.warning('Output folder:' + self.extractPath + 'already hold files')
 
-        elif self.warning_bool is False:
-            if filename == '*':
-                self.extractAll()
-            else:
-                print(os.path.join(self.downloadPath, filename))
-                self.extractFile(os.path.join(self.downloadPath, filename))
+    # TODO: Add more logging to this section
+    def extract(self, filename, extract_path, download_path):
+        self.download_path = download_path
+        self.extract_path = extract_path
+
+        if filename == '*':
+            self.extractAll()
+        else:
+            print(os.path.join(self.download_path, filename))
+            self.extractFile(os.path.join(self.download_path, filename))
 
     def extractFile(self, file):
-        if os.path.exists(file) and self.warning_bool is False:
+        if os.path.exists(file) :
             if file.endswith('tar.gz'):
                 self.Log.info("Opening tar.gz file:" + file)
                 tar = tarfile.open(file, 'r:gz')
@@ -43,19 +41,23 @@ class FileExtraction:
                 if not os.path.exists(finalPath):
                     self.Log.info('Creating directory:' + finalPath)
                     os.makedirs(finalPath)
-                self.Log.info('Extracting tar.gz to:' + finalPath)
-                tar.extractall(finalPath)
-                self.Log.info('Extraction complete')
-                print('Extraction complete')
-                tar.close()
-                print('Calculating hashes of extracted files')
-                self.calculateHash(finalPath, dirname)
-
+                    self.Log.info('Extracting tar.gz to:' + finalPath)
+                    tar.extractall(finalPath)
+                    self.Log.info('Extraction complete')
+                    print('Extraction complete')
+                    tar.close()
+                    print('Calculating hashes of extracted files')
+                    self.calculateHash(finalPath, dirname)
+                elif os.path.exists(finalPath):
+                    print('Warning the extraction folder already exists: ' + finalPath )
+                    print('Rename or move the folder and restart the script to extract the files')
+                    self.Log.warning('Folder:' + finalPath + 'exists')
+                    self.Log.warning('Stopping extraction script.')
 
     def extractAll(self):
-        listOfFile = os.listdir(self.downloadPath)
+        listOfFile = os.listdir(self.download_path)
         for entry in listOfFile:
-            fullPath = os.path.join(self.downloadPath, entry)
+            fullPath = os.path.join(self.download_path, entry)
             if os.path.isdir(fullPath):
                 print('dir')
             elif fullPath.endswith('tar.gz'):
@@ -64,9 +66,9 @@ class FileExtraction:
                 print('this is not a tar.gz file, skipping')
 
     def calculateHash(self, path, dirname):
-        hashFile = os.path.join(path, dirname + '.csv')
+        hashFile = os.path.join(path, dirname + 'hashes.csv')
         with open(hashFile, 'w') as file:
-            filewriter = csv.writer(file,delimiter=',',
+            filewriter = csv.writer(file, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
             filewriter.writerow(['Date', 'Filename', 'Hash'])
             for path, subdirs, files in os.walk(path):
@@ -75,4 +77,3 @@ class FileExtraction:
                     hash = main.Main().bereken_hash(os.path.join(path, name))
                     filewriter.writerow([str(strftime("%Y-%m-%d %H:%M:%S", gmtime())), os.path.join(path, name), str(hash)])
             # filewriter.close()
-
