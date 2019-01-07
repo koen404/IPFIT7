120d37313eb2436b044eae3d6157344937f14b77
koen <koenvandersijs@hotmail.com>
2019-01-07 12:59:07 +0100

    Added code so that the script will be able to load the correct maildir. Still need to add an option to select all maildirs in a folder.

--- Mail_analysis.py
+++ Mail_analysis.py
@@ -12,37 +12,40 @@ class Mail_analysis:
 
     def load_maildir(self, casedir_path):
         self.casedir_path = casedir_path
-        self.mailbak_path = os.path.join(casedir_path, 'imap')
-        dir_list = os.listdir(self.mailbak_path)
-        i = 0
-        for file in dir_list:
-            if os.path.isdir(file):
-                i+=1
-                print(str(i) + ':' + file)
+        self.mailbak_path = os.path.join(casedir_path)
+        nextfolder = self.listdir(self.mailbak_path, 'Please enter the index of the back-up of which you want to analyse the maildir')
+        nextfolder = os.path.join(nextfolder, 'imap')
+        print(nextfolder)
+        thirdfolder = self.listdir(nextfolder, 'Please enter the index of the domain you want to analyse the mail from')
+        self.maildir = self.listdir(thirdfolder, 'Please enter the index of the maildir you want to analyse')
+        print(self.maildir)
 
-
-        index_input = False
-        while index_input is False:
-            folder_index = input('Select of what domain you want to analyse the maildir:')
-            try:
-                val = int(folder_index)
-
-            except ValueError:
-                print('This is not a correct index please try again')
-
-            # index_input = False
-            # while index_input is False:
-            #     download_index = input("What file do you want to download?")
-            #     try:
-            #         val = int(download_index)
-            #         download_index = val
-            #         index_input = True
-            #     except ValueError:
-            #         self.Log.error("Wrong Index number")
-            #
-            # if download_index == 0:
-            #     continue
-        print('')
 
     def showmail(self):
         print('')
+
+    def listdir(self, path, input_question):
+        dir_list = os.listdir(path)
+        print(path)
+        print (len(dir_list))
+        i = 0
+        for file in dir_list:
+
+            if os.path.isdir(os.path.join(path,file)):
+                i += 1
+                print(str(i) + " " + file)
+
+        index_input = False
+        while index_input is False:
+            folder_index = input(input_question)
+            try:
+                val = int(folder_index)
+                index_input = True
+                if val > len(dir_list):
+
+                    index_input = False
+                    raise ValueError
+            except ValueError:
+                print('This is not a correct index please try again')
+        nextfolder = os.path.join(path, dir_list[val -1])
+        return nextfolder
