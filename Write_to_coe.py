a5becdf7b07bfdcef2c01a18cad86287d52a706e
koen <koenvandersijs@hotmail.com>
2019-01-07 15:49:08 +0100

    Function that write's info to the coe file.

---
+++ Write_to_coe.py
@@ -0,0 +1,9 @@
+from time import gmtime, strftime
+import csv
+
+
+def write_to_coe(coe_output_file, message , hash=''):
+    with open(coe_output_file, 'w') as file:
+        filewriter = csv.writer(file, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        filewriter.writerow([str(strftime("%Y-%m-%d %H:%M:%S", gmtime())), message, hash])
