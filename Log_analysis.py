767335249812871e02c052754111d2056b474626
koen <koenvandersijs@hotmail.com>
2019-01-08 21:37:12 +0100

    The function will now generate two lists instead of two list in a list of the IP-addresses.

--- Log_analysis.py
+++ Log_analysis.py
@@ -1,4 +1,6 @@
 import re
+
+
 # TODO: extract the IP-adressess from the log files. Check what IP's have logged in successfully
 def analyse_log(logfile):
     failed_ip = []
@@ -8,11 +10,14 @@ def analyse_log(logfile):
         with open(logfile, 'r') as file:
             for line in file:
                 if 'Accepted' in line:
-                    ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line)
+                    ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line).pop()
                     succesfull_ip.append(ip)
+
                 if 'Failed' in line:
-                    ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line)
+                    ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line).pop()
                     failed_ip.append(ip)
     print(failed_ip)
     print(succesfull_ip)
+    for line in succesfull_ip:
+        print(line)
     print('test')
