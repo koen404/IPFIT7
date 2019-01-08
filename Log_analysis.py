b36346fe26f72b6551b1d732053cefc98663e5a6
koen <koenvandersijs@hotmail.com>
2019-01-08 21:21:27 +0100

    The function will append the failed an successfull log in attempts in different lists.

--- Log_analysis.py
+++ Log_analysis.py
@@ -1,10 +1,18 @@
-
+import re
 # TODO: extract the IP-adressess from the log files. Check what IP's have logged in successfully
-def analyse_log(logfile, http_log=None):
-    if 'auth' in logfile:
+def analyse_log(logfile):
+    failed_ip = []
+    succesfull_ip = []
+    if 'secure' in logfile:
         print('analyzing auth.log')
-        for line in logfile:
-
-
-    elif 'secure' in logfile:
-        print('analyzing secure.log')
+        with open(logfile, 'r') as file:
+            for line in file:
+                if 'Accepted' in line:
+                    ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line)
+                    succesfull_ip.append(ip)
+                if 'Failed' in line:
+                    ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line)
+                    failed_ip.append(ip)
+    print(failed_ip)
+    print(succesfull_ip)
+    print('test')
