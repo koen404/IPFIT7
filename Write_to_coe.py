84c11a1b9f0a7d2c130a3460725aeae16ee844e1
koen <koenvandersijs@hotmail.com>
2019-01-09 12:06:26 +0100

    will now append to the coe file instead of overwriting it.

--- Write_to_coe.py
+++ Write_to_coe.py
@@ -2,8 +2,8 @@ from time import gmtime, strftime
 import csv
 
 
-def write_to_coe(coe_output_file, message , hash=''):
-    with open(coe_output_file, 'w') as file:
+def write_to_coe(coe_output_file, message, hash=''):
+    with open(coe_output_file, 'a') as file:
         filewriter = csv.writer(file, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
         filewriter.writerow([str(strftime("%Y-%m-%d %H:%M:%S", gmtime())), message, hash])
