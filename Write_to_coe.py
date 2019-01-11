c0d535424e603b0757bdb18a41423a604e1945c1
koen <koenvandersijs@hotmail.com>
2019-01-11 11:17:45 +0100

    Added get_output_file function to return the correct location of the coe file.

--- Write_to_coe.py
+++ Write_to_coe.py
@@ -1,9 +1,15 @@
 from time import gmtime, strftime
 import csv
+import os
 
-
-def write_to_coe(coe_output_file, message, hash=''):
-    with open(coe_output_file, 'a') as file:
+def write_to_coe(output_file, message, hash=''):
+    with open(output_file, 'a') as file:
         filewriter = csv.writer(file, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
         filewriter.writerow([str(strftime("%Y-%m-%d %H:%M:%S", gmtime())), message, hash])
+
+def get_coe_output(casedir):
+    abs_path = os.path.abspath(os.path.dirname(casedir))
+    os.chdir(abs_path)
+    coe_output_file = os.path.abspath('output')
+    return coe_output_file
