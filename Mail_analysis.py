d77556ae59883b0d17a8002dcd19765cf06ca06d
koen <koenvandersijs@hotmail.com>
2019-01-12 16:27:06 +0100

    Added some logging, also added code to the analyse function that will print out all mails that have an attachment.

--- Mail_analysis.py
+++ Mail_analysis.py
@@ -1,41 +1,63 @@
 import mailbox
 import email.utils
 import os
+import main
 
-
+# todo : make maildir filter out all mails with attachements.
 class Mail_analysis:
 
-    def __init__(self):
-        self.output = None
-        self.casedir_path = None
-        self.mailbak_path = None
-
-    def load_maildir(self, casedir_path):
+    def __init__(self, casedir_path):
         self.casedir_path = casedir_path
         self.mailbak_path = os.path.join(casedir_path)
-        nextfolder = self.listdir(self.mailbak_path, 'Please enter the index of the back-up of which you want to analyse the maildir')
-        nextfolder = os.path.join(nextfolder, 'imap')
-        print(nextfolder)
-        thirdfolder = self.listdir(nextfolder, 'Please enter the index of the domain you want to analyse the mail from')
-        self.maildir = self.listdir(thirdfolder, 'Please enter the index of the maildir you want to analyse')
-        print(self.maildir)
-        self.showmail(self.maildir)
+        self.log = main.Main().Log()
+    # let the user select the correct maildir
+    def load_maildir(self):
+        if os.listdir(self.mailbak_path):
+            nextfolder = self.listdir(self.mailbak_path, 'Please enter the index of the back-up of which you want to analyse the maildir')
+            nextfolder = os.path.join(nextfolder, 'imap')
+            print(nextfolder)
+            thirdfolder = self.listdir(nextfolder, 'Please enter the index of the domain you want to analyse the mail from')
+
+            self.fourthfolder = self.listdir(thirdfolder, 'Please enter the index of the maildir you want to analyse')
+            self.log.info('Selecting maildir folder' + self.maildir)
+            print(self.maildir)
+            self.showmail(self.maildir)
+        else:
+            print('The extraction folder is empty, please download and extract the back-up first')
+            self.log.warning('The extraction folder doesn\'t contain any files')
+
 
     # TODO test this function with a know good maildir,because it looks like this function doesn't work as it suposed to
+    # Write the mail to an csv file. And still needs some kind of analysis.
     def showmail(self, maildir):
-        mbox = mailbox.Maildir(os.path.join(maildir, 'Maildir','.Sent'))
+        mbox = mailbox.Maildir(os.path.join(maildir, 'Maildir'))
         mbox.lock()
         for message in mbox:
+            if message.get_content_maintype() == 'multipart':
+                print(message['subject'])
+                print(message['To'])
+                print(message['From'])
+                print(message['Date'])
+
+                for part in message.walk():
+                    if part.get_content_maintype() == 'multipart':
+                        continue
+                    if part.get('Content-Disposition') is None:
+                        continue
+                    filename = part.get_filename()
+                    print(filename)
+
             print(message)
         print('')
 
+    # list the content of a selected dir.
+    # This function will be used to loop through the different back-up folders to finally select the maildir.
     def listdir(self, path, input_question):
         dir_list = os.listdir(path)
-        print(path)
         i = 0
         for file in dir_list:
 
-            if os.path.isdir(os.path.join(path,file)):
+            if os.path.isdir(os.path.join(path, file)):
                 i += 1
                 print(str(i) + " " + file)
 
@@ -49,7 +71,12 @@ class Mail_analysis:
 
                     index_input = False
                     raise ValueError
+                nextfolder = os.path.join(path, dir_list[val - 1])
+                index_input = True
             except ValueError:
                 print('This is not a correct index please try again')
-        nextfolder = os.path.join(path, dir_list[val -1])
+                self.log.warning('Incorrect index input for mail dir folder by user')
+                index_input = False
+                return None
+
         return nextfolder
