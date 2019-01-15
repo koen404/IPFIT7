8ac83324404dd5e6f8ceee221966c56db3cb56a3
koen <koenvandersijs@hotmail.com>
2019-01-15 13:33:54 +0100

    You can now select the .sql file to be restored.

--- Restore_database.py
+++ Restore_database.py
@@ -1,19 +1,30 @@
 import pymysql
 import os
 import main
-
+from resources import list_dir
 
 class database_analysis:
-    def __init__(self):
+    def __init__(self, extract_path):
         self.database_name = None
-        self.Log =  main.Main.Log()
+        self.Log = main.Main().Log()
+        self.extract_path = extract_path
+
+    def select_database(self):
+        input_question = 'Select the backup of which you want to restore the database'
+
+        first = list_dir.listdir(self.extract_path, input_question, files=True)
+
+        while not first.endswith('.sql'):
+            first = list_dir.listdir(first, input_question, files=True)
+        if first.endswith('.sql'):
+            self.import_database(first)
 
     def import_database(self, database_name):
         host = '127.0.0.1'
         user = 'IPFIT7'
         output_database = os.path.basename(database_name)
         output_database = output_database.replace('.sql', '')
-        passwd= input('please enter the password of the root user')
+        passwd= input('please enter the password of the IPFIT7 user')
         self.con = pymysql.connect(host, user, passwd)
         self.Log.info('Connecting to Local database')
         try:
