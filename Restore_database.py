377a6b4ea3680a300494493f5de883ba05f33e54
koen <koenvandersijs@hotmail.com>
2019-01-15 15:01:07 +0100

    Will now instead of reading the back-up file in python use the mysql command of linux to restore the database.

--- Restore_database.py
+++ Restore_database.py
@@ -2,6 +2,7 @@ import pymysql
 import os
 import main
 from resources import list_dir
+import subprocess
 
 class database_analysis:
     def __init__(self, extract_path):
@@ -25,22 +26,22 @@ class database_analysis:
         output_database = os.path.basename(database_name)
         output_database = output_database.replace('.sql', '')
         passwd= input('please enter the password of the IPFIT7 user')
-        self.con = pymysql.connect(host, user, passwd)
+        try:
+            self.con = pymysql.connect(host, user, passwd)
+        except (pymysql.OperationalError) as e:
+            self.Log.error('An error occurred while connecting to the database:' + e)
+
         self.Log.info('Connecting to Local database')
         try:
             mycursor =self.con.cursor()
+
         except (pymysql.MySQLError, pymysql.Warning) as e:
             print(e)
             self.Log.error('MySQL connection error: ' + e)
             return None
 
-        mycursor.execute("CREATE DATABASE IF NOT EXISTS" + output_database)
-        with mycursor as cursor:
-            for line in open(database_name, 'r'):
-                if line.strip():
-                    cursor.execute(line)
-                    self.Log.info('Executing Database query:' + line)
-
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS " + output_database)
+        subprocess.call(["mysql", "-u", user, "-p",passwd, output_database , '<', database_name])
 
     def run_mysqlWB(self):
         os.system('mysql-workbench')
