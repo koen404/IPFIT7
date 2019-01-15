3bffa0fa538d93987e049dfde8bf309e3ebcd03b
koen <koenvandersijs@hotmail.com>
2019-01-15 12:50:14 +0100

    Added some comments, also the COE function will now check if the examiner name is different then the first added examiner.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import sys
 from datetime import date
 import logging
-import datetime
 import hashlib as hash
 import DA_backup
 import File_extraction
@@ -14,6 +13,7 @@ import getpass
 class Main:
     BUFFERSIZE = 65536
     extract_path = None
+
     def __init__(self):
         log_path = os.path.join(sys.path[0], "log")
         self.sha256hash = hash.sha256()
@@ -27,8 +27,9 @@ class Main:
             os.makedirs(log_path)
         if not os.path.exists(self.case_path):
             os.makedirs(self.case_path)
-
+        # this variable will contain the log location
         self.log_location = os.path.join(sys.path[0], 'log', 'Main_log_' + str(date.today()) + '.log')
+        # this will map the choices made by the user to the corresponding function
         self.choices_main = {
                 "1": self.create_back_up,
                 "2": self.download_back_up,
@@ -39,6 +40,7 @@ class Main:
 
                 }
 
+    # move the log function to a separate file/class
     def Log(self):
 
         logger = logging.getLogger(__name__)
@@ -52,7 +54,7 @@ class Main:
             handler.setFormatter(formatter)
             logger.addHandler(handler)
         return logger
-
+    # this function will display the tekst based userinterface
     def display_menu(self):
         os.system('cls' if os.name == 'nt' else 'clear')
         print("""
@@ -64,7 +66,7 @@ class Main:
         5. Download_logs
         6. Quit
         """)
-
+    # This function will calculate the hash of a file (can be moved to an separate file/class)
     def bereken_hash(self, file):
         if os.path.isfile(file) and os.access(file, os.R_OK):
             with open(file, 'rb') as file:
@@ -76,7 +78,7 @@ class Main:
             self.Log().info(file + ": File does not exist or is not accessible, will not calculate hash")
 
         return str(self.sha256hash.hexdigest())
-
+    # This function will be executed once the main script is run.
     def run(self):
 
         self.coe()
@@ -90,36 +92,39 @@ class Main:
             os.makedirs(self.server_log_path)
 
         self.Log().info("Starting Main_Script")
-
+        self.DA = DA_backup.DA_backup(self.casedir)
         while True:
             self.display_menu()
             choice = input("Enter an option: ")
             action = self.choices_main.get(choice)
             self.Log().info("User input: %s", choice)
-            self.DA = DA_backup.DA_backup()
+
             if action:
                 action()
             else:
                 self.Log().info("Invalid input, restarting script")
                 print("{0} is not a valid choice".format(choice))
 
+    # This function will call the back-up function from the DA_back-up class.
     def create_back_up(self):
         try:
             if self.sshpassword is not None:
-                self.DA.back_up(self.host, self.sshusername, self.sshpassword, self.coe_output_file, self.backupuser)
+                self.DA.back_up(self.host, self.sshusername, self.sshpassword, self.backupuser)
         except AttributeError as e:
             self.sshusername = 'koen'
             self.host = input('please enter the DirectAdmin host: ')
             self.sshpassword = input('Please enter the SSH password: ')
             self.backupuser = input('Please enter the user of which you want to create the back-up from:')
-            self.DA.back_up(self.host, self.sshusername, self.sshpassword,  self.coe_output_file, self.backupuser)
+            self.DA.back_up(self.host, self.sshusername, self.sshpassword, self.backupuser)
 
+    # This function will call the download function from the DA_back-up class
     def download_back_up(self):
         username = 'admin'
         ftppassword = input('FTP_password:')
         host = input('please enter host')
-        self.DA.download_backup(username, ftppassword, host, self.download_path, self.coe_output_file)
+        self.DA.download_backup(username, ftppassword, host, self.download_path)
 
+    # This function will call the download log function from the DA_Back-up class
     def download_logs(self):
         try:
             if self.sshpassword is not None:
@@ -129,46 +134,69 @@ class Main:
             self.host = input('please enter the DirectAdmin host: ')
             self.sshpassword = input('Please enter the SSH password: ')
             self.DA.download_log(self.sshusername, self.sshpassword, self.host, self.server_log_path)
-
+    # This function
     def file_extraction(self):
         filename = input("please enter the tar.gz file you want to extract (press * for all):")
         print(self.extract_path)
         print(self.download_path)
         File_extraction.FileExtraction().extract(filename, self.extract_path, self.download_path, self.coe_output_file)
 
+    # This function will create the COE file. If it already exists it will use the existing file.
     def coe(self):
         self.examiner = input('Please enter your name:')
         self.casenumber = input('Please enter the casenumber:')
         self.casename = input('please enter a case name (press enter if none):')
+
         self.casedir = os.path.join(self.case_path, self.casenumber + '_' + self.casename)
         if not os.path.exists(self.casedir):
+            self.Log().info('Create casedir: ' + self.casedir)
             os.makedirs(self.casedir)
+
         self.coe_output_file = os.path.join(self.casedir, 'coe.csv')
         if not os.path.exists(self.coe_output_file):
-            with open(self.coe_output_file, 'w') as file:
+            with open(self.coe_output_file, 'a') as file:
                 filewriter = csv.writer(file, delimiter=',',
                                         quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                filewriter.writerow(['Examiner', self.examiner])
-                filewriter.writerow(['Case number', self.casenumber])
-                filewriter.writerow(['Case Name', self.casename])
+                filewriter.writerow(['Examiner:', self.examiner])
+                filewriter.writerow(['Case number:', self.casenumber])
+                filewriter.writerow(['Case Name:', self.casename])
+                filewriter.writerow()
                 filewriter.writerow(['When', 'What', 'Hash'])
         else:
-            print('Using existing coe file: ' + self.coe_output_file)
+            i = 0
+            with open(self.coe_output_file, 'r') as file:
+                readCSV = csv.reader(file, delimiter=',',
+                                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                for row in readCSV:
+                    if i ==0:
+                        temp_name = row[1]
+                        print(temp_name)
 
-    # TODO: create a maildir analysis script
+                    i+=1
+            temp_name = str(temp_name).upper()
+            if self.examiner.upper() == temp_name:
+                self.Log().info('COE file already exist')
+            else:
+                with open(self.coe_output_file, 'a') as file:
+                    filewriter = csv.writer(file, delimiter=',',
+                                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                    filewriter.writerow()
+                    filewriter.writerow(['New Examiner', self.examiner])
+
+    # This function will run the mail analysis class
     def mail_analysis(self):
         print('mail analysis')
-        Mail_analysis.Mail_analysis().load_maildir(self.extract_path)
+        Mail_analysis.Mail_analysis(self.extract_path).load_maildir()
 
-
-    #TODO: create a script to analyse .sql files
+    # This function will run the database restore and analysis function from the restore_database class
     def database_analysis(self):
         print('Database analysis')
 
+    # This function will stop the program from running
     def quit(self):
         self.Log().info("Exiting script")
         sys.exit(0)
 
-
+# This if statement will cause the run function to keep running
 if __name__ == "__main__":
     Main().run()
