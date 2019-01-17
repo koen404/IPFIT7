40f37e62bcffa8c26e59be06c843881c359ad7e4
koen <koenvandersijs@hotmail.com>
2019-01-17 15:00:27 +0100

    Added some comments, also the COE function will now check if the examiner name is different then the first added examiner.

--- Restore_database.py
+++ Restore_database.py
@@ -18,30 +18,38 @@ class database_analysis:
         while not first.endswith('.sql'):
             first = list_dir.listdir(first, input_question, files=True)
         if first.endswith('.sql'):
-            self.import_database(first)
+            self.convert_database(first)
 
-    def import_database(self, database_name):
-        host = '127.0.0.1'
-        user = 'IPFIT7'
+    def convert_database(self, database_name):
+
         output_database = os.path.basename(database_name)
-        output_database = output_database.replace('.sql', '')
-        passwd= input('please enter the password of the IPFIT7 user')
-        try:
-            self.con = pymysql.connect(host, user, passwd)
-        except (pymysql.OperationalError) as e:
-            self.Log.error('An error occurred while connecting to the database:' + e)
 
-        self.Log.info('Connecting to Local database')
-        try:
-            mycursor =self.con.cursor()
+        temp_path = os.path.abspath(self.extract_path)
+        outpath = os.path.join(temp_path, "../output/database")
+        print(outpath)
+        if not os.path.exists(outpath):
+            os.makedirs(outpath)
+            self.Log.info('Creating directory : ' + outpath)
 
-        except (pymysql.MySQLError, pymysql.Warning) as e:
-            print(e)
-            self.Log.error('MySQL connection error: ' + e)
-            return None
+        output_database = os.path.join(outpath, output_database.replace('.sql', '.db'))
+        subprocess.call("./mysql2sqlite", database_name, '|', output_database)
+        # passwd= input('please enter the password of the IPFIT7 user')
+        # try:
+        #     self.con = pymysql.connect(host, user, passwd)
+        # except (pymysql.OperationalError) as e:
+        #     self.Log.error('An error occurred while connecting to the database:' + e)
+        #
+        # self.Log.info('Connecting to Local database')
+        # try:
+        #     mycursor =self.con.cursor()
+        #
+        # except (pymysql.MySQLError, pymysql.Warning) as e:
+        #     print(e)
+        #     self.Log.error('MySQL connection error: ' + e)
+        #     return None
+        #
+        # mycursor.execute("CREATE DATABASE IF NOT EXISTS " + output_database)
+        # subprocess.call(["mysql", "-u", user, "-p",passwd, output_database , '<', database_name])
 
-        mycursor.execute("CREATE DATABASE IF NOT EXISTS " + output_database)
-        subprocess.call(["mysql", "-u", user, "-p",passwd, output_database , '<', database_name])
-
-    def run_mysqlWB(self):
-        os.system('mysql-workbench')
+    def run_sqliteBrowser(self):
+        os.system('')
