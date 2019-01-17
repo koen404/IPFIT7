2e36037d5683419539a8c4698ab18e667c118ec7
koen <koenvandersijs@hotmail.com>
2019-01-17 21:24:59 +0100

    Will now output the coe file to the right output file

--- resources/Write_to_coe.py
+++ resources/Write_to_coe.py
@@ -10,7 +10,11 @@ def write_to_coe(output_file, message, hash=''):
         filewriter.writerow([str(strftime("%Y-%m-%d %H:%M:%S", gmtime())), message, hash])
 
 def get_coe_output(casedir):
-    abs_path = os.path.abspath(os.path.dirname(casedir))
-    os.chdir(abs_path)
-    coe_output_file = os.path.abspath('output')
+    temp = os.path.join(casedir)
+    if not os.path.exists(temp):
+        os.makedirs(temp)
+
+
+    coe_output_file = os.path.join(temp, 'coe.csv')
+    print(coe_output_file)
     return coe_output_file
