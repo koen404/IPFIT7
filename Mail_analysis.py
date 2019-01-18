1975d9d9229c95a8af5b20e2328667b6594d3ac8
koen <koenvandersijs@hotmail.com>
2019-01-18 15:31:40 +0100

    Added choice to either filter out the send or received mail, and to filter out only the mails with attachments or al mails.

--- Mail_analysis.py
+++ Mail_analysis.py
@@ -4,7 +4,6 @@ import main
 from resources import list_dir
 import csv
 
-
 class Mail_analysis:
 
     def __init__(self, casedir_path):
@@ -25,74 +24,34 @@ class Mail_analysis:
             self.output_name = os.path.basename(self.maildir)
             self.log.info('Selecting maildir folder' + self.maildir)
             print(self.maildir)
-            self.showmail(self.maildir)
+            self.show_mail(self.maildir)
         else:
             # Give warning when the extraction folder is empty
             print('The extraction folder is empty, please download and extract the back-up first')
             self.log.warning('The extraction folder doesn\'t contain any files')
 
     # Write the mail to an csv file. And still needs some kind of analysis.
-    def showmail(self, maildir):
-        self.attachment_output = os.path.join(self.output_path, 'attachments')
-        # create the attachment output folder.
-        if not os.path.exists(self.attachment_output):
-            os.makedirs(self.attachment_output)
+    def show_mail(self, maildir):
+
         # open the sent maildir
+
+
         mbox = mailbox.Maildir(os.path.join(maildir, 'Maildir', '.Sent'))
-        mbox.lock()
 
         index_input = False
-        val = 0
-        while index_input is False:
-            # select if all sent mails must be written to the csv file or only the mails with attachments
-            print('1. Filter out all sent mails')
-            print('2. Filter only sent mails with attachments.')
-            print('')
-            attachment = input('Extract all sent mails or only with attachment?')
-            try:
-                val = int(attachment)
-                index_input = True
-                if val > 2 or val <= 0:
-                    index_input = False
-                    raise ValueError
-                index_input = True
-            except ValueError:
-                print('This is not a correct input please try again')
+        val = self.get_input('1. Filter out sent mails','2. Filter out received mails','Filter out sent or received?')
 
-                index_input = False
-                return None
-        i = 1
-        counter = 1
-        for message in mbox:
-            if val == 1:
-                if message is not None:
-                    self.write_mail(message, counter)
-                    counter += 1
-                else:
-                    print("No e-mails found")
-                    self.log.info("No e-mail messages found")
+        sent = False
+        if val == 1:
+            sent = True
+        elif val == 2:
+            sent = False
+        self.analyse_mails(maildir, sent)
 
-            if message.get_content_maintype() == 'multipart' and val == 2:
-                self.write_mail(message, counter)
-                counter += 1
-
-                for part in message.walk():
-                    if part.get_content_maintype() == 'multipart':
-                        continue
-                    if part.get('Content-Disposition') is None:
-                        continue
-                    filename = part.get_filename()
-                    print(filename)
-                    fb = open(os.path.join(self.attachment_output, i+filename), 'w')
-                    i +=1
-                    fb.write(part.get_payload(decode=True))
-                    fb.close()
-            else:
-                print("No e-mails found")
-                self.log.info("No e-mail message found")
 
     # Write the mail to a CSV file
-    def write_mail(self, message, message_nr):
+    def write_mail(self, message, message_nr, output_name):
+        self.output_name = self.output_name + output_name
         with open(os.path.join(self.output_path, self.output_name+'.csv'), 'a') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(['-------------------------------------'])
@@ -115,3 +74,71 @@ class Mail_analysis:
                 elif "\\" in temp:
                     continue
             writer.writerow(['-------------------------------------'])
+
+    def analyse_mails(self, maildir, sent=False):
+        if sent==True:
+            mbox = mailbox.Maildir(os.path.join(maildir, 'Maildir', '.Sent'))
+            output_name = 'Sent'
+        else:
+            mbox = mailbox.Maildir(os.path.join(maildir, 'Maildir'))
+            output_name = 'received'
+
+        self.attachment_output = os.path.join(self.output_path, 'attachments')
+        # create the attachment output folder.
+        if not os.path.exists(self.attachment_output):
+            os.makedirs(self.attachment_output)
+
+        mbox.lock()
+        val = self.get_input('1. Filter out all  mails', '2. Filter only mails with attachments.', 'Extract all sent mails or only with attachment?')
+
+        i = 1
+        counter = 1
+        for message in mbox:
+            if val == 1:
+
+                if message is not None:
+                    self.write_mail(message, counter, output_name)
+                    counter += 1
+                else:
+                    print("No e-mails found")
+                    self.log.info("No e-mail messages found")
+
+            if message.get_content_maintype() == 'multipart' and val == 2:
+                output_name+='_at'
+                self.write_mail(message, counter, output_name)
+                counter += 1
+
+                for part in message.walk():
+                    if part.get_content_maintype() == 'multipart':
+                        continue
+                    if part.get('Content-Disposition') is None:
+                        continue
+                    filename = part.get_filename()
+                    print(filename)
+                    fb = open(os.path.join(self.attachment_output, i + filename), 'w')
+                    i += 1
+                    fb.write(part.get_payload(decode=True))
+                    fb.close()
+            else:
+                print("No e-mails found")
+                self.log.info("No e-mail message found")
+
+    def get_input(self, question, question2, input_question):
+        index_input = False
+        val = 0
+        while index_input is False:
+            # select if all sent mails must be written to the csv file or only the mails with attachments
+            print(question)
+            print(question2)
+            print('')
+            attachment = input(input_question)
+            try:
+                val = int(attachment)
+                if val > 2 or val <= 0:
+                    raise ValueError
+                else:
+                    return val
+            except ValueError:
+                print('This is not a correct input please try again')
+
+                index_input = False
