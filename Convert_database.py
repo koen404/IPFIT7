b4f4bd9341f045a3484dc5b110dad75b80a59339
koen <koenvandersijs@hotmail.com>
2019-01-18 17:15:35 +0100

    Added comments, also made sure that the convert script will run correctly so I let the script change directory before running the conversion script.

--- Convert_database.py
+++ Convert_database.py
@@ -1,16 +1,19 @@
-import pymysql
 import os
 import main
 from resources import list_dir
 import subprocess
 
+
 class database_analysis:
-    def __init__(self, extract_path):
+    def __init__(self, extract_path, casedir):
+        self.casedir = casedir
         self.database_name = None
         self.Log = main.Main().Log()
         self.extract_path = extract_path
 
+    # function to let the user select the database that needs to be converted and loaded in the SQLite browser
     def select_database(self):
+
         input_question = 'Select the backup of which you want to restore the database'
 
         first = list_dir.listdir(self.extract_path, input_question, files=True)
@@ -20,12 +23,15 @@ class database_analysis:
         if first.endswith('.sql'):
             self.convert_database(first)
 
+    # this function will convert the specified database dump to SQLite format
     def convert_database(self, database_name):
 
         output_database = os.path.basename(database_name)
 
-        temp_path = os.path.abspath(self.extract_path)
-        outpath = os.path.join(temp_path, "../output/database")
+        temp_path = os.path.abspath(self.casedir)
+
+        outpath = os.path.join(temp_path, "/output/database")
+        os.chdir(os.path.abspath(os.path.join(temp_path, '../', '../')))
         print(outpath)
         if not os.path.exists(outpath):
             os.makedirs(outpath)
@@ -52,6 +58,7 @@ class database_analysis:
         # mycursor.execute("CREATE DATABASE IF NOT EXISTS " + output_database)
         # subprocess.call(["mysql", "-u", user, "-p",passwd, output_database , '<', database_name])
 
+    # this function will open the specified SQLite database in the SQLite browser
     def run_sqliteBrowser(self, database):
         subprocess.call(['sqlitebrowser', '-R', database])
 
