d5b0bba16833dca184398d8cddd323c84becda15
koen <koenvandersijs@hotmail.com>
2019-01-19 23:18:07 +0100

    Added comments

--- resources/Write_to_coe.py
+++ resources/Write_to_coe.py
@@ -2,6 +2,8 @@ from time import gmtime, strftime
 import csv
 import os
 
+
+# This function will write the message and hash to the specified output file
 def write_to_coe(output_file, message, hash=''):
     print(output_file)
     with open(output_file, 'a') as file:
@@ -9,11 +11,12 @@ def write_to_coe(output_file, message, hash=''):
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
         filewriter.writerow([str(strftime("%Y-%m-%d %H:%M:%S", gmtime())), message, hash])
 
+
+# function that return the coe output path based on the casedir path
 def get_coe_output(casedir):
     temp = os.path.join(casedir)
     if not os.path.exists(temp):
         os.makedirs(temp)
-
 
     coe_output_file = os.path.join(temp, 'coe.csv')
     print(coe_output_file)
