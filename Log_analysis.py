91f2c41760ec75d560cef76cb6be15f6ff16f7a8
koen <koenvandersijs@hotmail.com>
2019-01-26 12:04:19 +0100

    Added some comments and removed obsolete function.

--- Log_analysis.py
+++ Log_analysis.py
@@ -16,29 +16,30 @@ class Log:
         abs_path = os.path.abspath(os.path.dirname(logfile))
         os.chdir(abs_path)
         output_path = os.path.abspath('../output/Logs')
-
+        # create the output directory if it doesn't exists
         if not os.path.exists(output_path):
             os.makedirs(output_path)
             self.Log.info('Creating directory: ' + output_path)
-
+        # list for failed and successful logged in IP's
         failed_ip = []
         successfull_ip = []
+        # output file location
         output_file = os.path.join(output_path, filename)
         if os.path.exists(output_file) and auth is False:
             output_file = uniquify.uniquify(output_file)
             self.Log.info('Set output file to' + output_file)
 
         with open(logfile, 'r') as file:
+            # check if it is the secure access or auth log
             if 'secure' in logfile:
                 for line in file:
+                    # check if it is a failed or an successful login
                     if 'Accepted' in line:
                         print (line)
                         ip = self.getIP(line)
                         successfull_ip.append(ip)
                         line = line.replace(ip, 'IP')
-                        # (Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\s+\d{1,2} (?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)
-                        # (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d{1,2} (?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)
-                        #([0-2][0-9]|(3)[0-1])(\/)(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)(\/)\d{4}:(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)|(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d{1,2} (?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)
+                        # regex to extract the date notation from the log file.
                         date = re.search('((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d{1,2} (?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d))',line).group()
                         line = line.replace(date, '')
                         self.output_log(output_file, ip, date, line)
@@ -53,10 +54,12 @@ class Log:
                 self.write_ip(failed_ip, output_file, 'Failed_logged_in_IP\'s')
             elif 'access' in logfile:
                 for line in file:
+                    # check if someone logged in successfully to PHPMyAdmin
                     if 'POST /phpmyadmin/ajax.php' in line:
                         ip = self.getIP(line, pop_index=0)
                         print(ip)
                         line = line.replace(ip, '')
+                        # regex to extract the date from the log file
                         date = re.search('(([0-2][0-9]|(3)[0-1])(\/)(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)(\/)\d{4}:(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d))', line).group()
                         line = line.replace(date, '')
                         self.output_log(output_file, ip, date, line)
@@ -71,7 +74,6 @@ class Log:
                     line = line.replace(date, '')
                 self.write_ip(failed_ip, output_file, 'Failed IP')
 
-
     def output_log(self, file,  IP, date='', log_message='', log_message3=''):
         if not os.path.exists(file):
             output = open(file, 'w')
@@ -85,6 +87,7 @@ class Log:
                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
             filewriter.writerow([date, IP, log_message, log_message3])
 
+    # function to extract the IP-address from a log line
     def getIP(self, line, pop_index=None):
         if pop_index is None :
             ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line).pop()
@@ -92,12 +95,7 @@ class Log:
             ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line).pop(pop_index)
         return ip
 
-    def getdate(self, line):
-        date = re.search(
-            '((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d{1,2} (?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d))',
-            line).group()
-        return date
-
+    # write the IP address list to a file
     def write_ip(self, ip_list, output, message):
 
         ip_oc = Counter(ip_list)
