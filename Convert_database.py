4a62e7ca69783ee0c8ddb274935419646b34864d
koen <koenvandersijs@hotmail.com>
2019-01-26 14:47:46 +0100

    Added comments, also removed obsolete imports and code.

--- Convert_database.py
+++ Convert_database.py
@@ -1,6 +1,5 @@
 import os
 import main
-from resources import list_dir
 import subprocess
 import tkinter
 from resources import Write_to_coe
@@ -18,6 +17,7 @@ class database_analysis:
 
     # function to let the user select the database that needs to be converted and loaded in the SQLite browser
     def select_database(self):
+        # This will generate an explorer window.
         root = tkinter.Tk()
         root.withdraw()
         root.overrideredirect(True)
@@ -27,9 +27,9 @@ class database_analysis:
         root.attributes("-topmost", True)
         root.focus_force()
         database_location = os.path.join(self.casedir, 'extract')
-        first = askopenfilename(initialdir=database_location, filetypes=[('Sql files', '*.sql')], parent = root)
+        first = askopenfilename(initialdir=database_location, filetypes=[('Sql files', '*.sql')], parent=root)
         print(first)
-
+        # check if the user has selected a file or pressed cancel.
         if first is None or not os.path.exists(first):
             print('Selection cancelled by user')
             self.Log.warning('Selection of database cancelled by user')
@@ -42,22 +42,21 @@ class database_analysis:
 
     # this function will convert the specified database dump to SQLite format
     def convert_database(self, database_name):
-
+        # Write coe info to the coe output file.
         Write_to_coe.write_to_coe(self.coe_output_file, 'Converting database to sqlite:' + database_name)
         output_database = os.path.basename(database_name)
-
+        # will create the absolute path of the casedir.
         temp_path = os.path.abspath(self.casedir)
-
+        # This is the output folder
         outpath = os.path.join(temp_path, "output", "database")
         Write_to_coe.write_to_coe(self.coe_output_file, 'Writing sqlite database to:' + outpath)
         os.chdir(os.path.abspath(os.path.join(temp_path, '..', '..', 'resources')))
         if not os.path.exists(outpath):
             os.makedirs(outpath)
             self.Log.info('Creating directory : ' + outpath)
-
+        # This will set the output path, the spaces will be escaped to prevent issues in Linux.
         temp = os.path.join(outpath, output_database.replace('.sql', '.db')).replace(' ', '\\ ')
-        print(temp)
-        print(os.getcwd())
+        # This will execute the conversion script.
         os.system("./mysql2sqlite " + database_name.replace(' ', '\\ ') + ' | sqlite3 ' + temp)
         self.run_sqliteBrowser(temp)
 
